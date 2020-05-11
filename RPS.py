ae06eb94c5bda208b771fe9977f5f227473473eb
Janna Pierre <gojanna@aol.com>
2020-05-11 18:06:00 -0400

    Added input and conditional statement

--- RPS.py
+++ RPS.py
@@ -1,4 +1,20 @@
 player_score = 0
 computer_score = 0
+choice = ["rock", "paper", "scissors"]
+
+input("Rock, Paper, Scissors?")
+
+while player_score and computer_score <=3:
+    if choice == "rock":
+        print("Rock")
+    elif choice == "paper":
+        print("Paper")
+    elif choice == "scissors":
+        print("Scissors")
+    else:
+        print("Select an option")
+
+
+
 
 
