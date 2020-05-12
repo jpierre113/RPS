9d30f5ddf445d252b4836ed8b0d87825caa51aa1
Janna Pierre <gojanna@aol.com>
2020-05-11 20:13:54 -0400

    Removed choice array, assigned it as input variable

--- RPS.py
+++ RPS.py
@@ -1,10 +1,11 @@
 player_score = 0
 computer_score = 0
-choice = ["rock", "paper", "scissors"]
 
-input("Rock, Paper, Scissors?")
 
-while player_score and computer_score <=3:
+choice = input("Rock, Paper, Scissors?")
+print(choice)
+
+while player_score and computer_score <= 3:
     if choice == "rock":
         print("Rock")
     elif choice == "paper":
