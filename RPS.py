27bc511fa662dadf5b99236bcf9560cd9d7aaa0f
Janna Pierre <gojanna@aol.com>
2020-05-11 20:39:03 -0400

    Imported random

--- RPS.py
+++ RPS.py
@@ -1,6 +1,6 @@
 # player_score = 0
 # computer_score = 0
-
+import random
 
 print("Rock Paper Scissors Game")
 print("Select your options: 1 for Rock, 2 for Paper and 3 for Scissors")
@@ -11,6 +11,9 @@ elif choice == 2:
     print("Paper has been selected")
 else:
     print("Scissors has been selected")
+
+print("The computer has chosen:")
+print(random.randint(1,3))
 
 
 
